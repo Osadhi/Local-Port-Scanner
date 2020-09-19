2e4989cc470cabfe1cbc91de89906ad5e037e661
Osadhi Virochana Jayasinghe Siriwardana <osadhiv@gmail.com>
2020-09-19 11:25:38 +0530

    Added Net Discover mode

--- port-scanner.py
+++ port-scanner.py
@@ -5,66 +5,87 @@ from queue import Queue
 from threading import Lock, Thread
 from time import sleep, time
 
-parser = OptionParser()
-parser.add_option("-H", "--host", dest="host", help="host address")
-(options, arguments) = parser.parse_args()
-socket.setdefaulttimeout(0.25)
-print_lock = Lock()
-q = Queue()
+from netdiscover import NetDiscover
 
-
-def port_scan(port):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        s.connect((t_IP, port))
-        with print_lock:
-            print(f'{port}\t open')
-        s.close()
-    except:
-        pass
-
-
-def threader():
-    while True:
-        port_g = q.get()
-        port_scan(port_g)
-        q.task_done()
-
-
-print(r"""
+banner = r"""
     ____             __     _____                                 
    / __ \____  _____/ /_   / ___/_________ _____  ____  ___  _____
   / /_/ / __ \/ ___/ __/   \__ \/ ___/ __ `/ __ \/ __ \/ _ \/ ___/
  / ____/ /_/ / /  / /_    ___/ / /__/ /_/ / / / / / / /  __/ /    
 /_/    \____/_/   \__/   /____/\___/\__,_/_/ /_/_/ /_/\___/_/     
 
-""")
-target = options.host
-if not target:
-    target = input('Enter the host to be scanned: ')
+"""
 
-try:
-    t_IP = socket.gethostbyname(target)
-except socket.gaierror:
-    print("Hostname could not be resolved")
-    sleep(3)
-    sys.exit()
 
-except socket.error:
-    print("Couldn't connect to server")
-    sleep(3)
-    sys.exit()
+def getparser():
+    parser = OptionParser()
+    parser.add_option("-H", "--host", dest="host", help="host address")
+    parser.add_option("-a", "--auto", dest="auto", help="Auto discover addresses", default=False, action="store_true")
+    return parser.parse_args()
 
-print(f'Starting scan on host: {t_IP}\n')
-print('Port\tStatus')
-startTime = time()
 
-for _ in range(100):
-    t = Thread(target=threader, daemon=True)
-    t.start()
+class Scan:
+    def __init__(self):
+        socket.setdefaulttimeout(0.25)
+        self.print_lock = Lock()
+        self.q = Queue()
+        try:
+            self.t_IP = socket.gethostbyname(target)
+        except socket.gaierror:
+            print("\nHostname could not be resolved")
+            sleep(3)
+            sys.exit()
 
-for port_p in range(1, 1024):
-    q.put(port_p)
+        except socket.error:
+            print("\nCouldn't connect to server")
+            sleep(3)
+            sys.exit()
 
-q.join()
-print('\nTime taken:', str(int(time() - startTime)) + 's')
+        print(f'\n\n___________________________________________\nStarting scan on host: {self.t_IP}\n'
+              '___________________________________________\nPort\t\tStatus\n'
+              '-------------------------------------------')
+        self.startTime = time()
+
+        for _ in range(100):
+            t = Thread(target=self.threader, daemon=True)
+            t.start()
+
+        for port_p in range(1, 1024):
+            self.q.put(port_p)
+
+        self.q.join()
+        print('\nTime taken:', str(int(time() - self.startTime)) + 's')
+
+    def port_scan(self, port):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            s.connect((self.t_IP, port))
+            with self.print_lock:
+                print(f'{port}\t\t open')
+            s.close()
+        except:
+            pass
+
+    def threader(self):
+        while True:
+            port_g = self.q.get()
+            self.port_scan(port_g)
+            self.q.task_done()
+
+
+if __name__ == '__main__':
+    (options, arguments) = getparser()
+    print(banner)
+    auto = options.auto
+    if auto:
+        print('Using Auto Discover mode\n')
+        n = NetDiscover().result
+        for i in n:
+            target = i['ip']
+            Scan()
+
+    else:
+        target = options.host
+        if not target:
+            target = input('Enter the host to be scanned: ')
+        Scan()
