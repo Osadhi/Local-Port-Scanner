a394dc936f2ef0c2024697a9f02e37f7da28a8fa
Osadhi <osadhiv@gmail.com>
2020-09-19 20:09:14 +0530

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Port-Scanner',
+    version='0.1',
+    packages=[''],
+    url='https://github.com/Osadhi/Port-Scanner',
+    license='',
+    author='Osadhi Virochana',
+    author_email='osadhiv@gmail.com',
+    description='Simple port scanner with python'
+)
